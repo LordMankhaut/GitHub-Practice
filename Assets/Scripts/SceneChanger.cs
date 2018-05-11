c196c515f643851a1fbb54bfe6d9c4e3b41bc4fe
LordMankhaut <bdalcorn@outlook.com>
2018-05-11 21:01:45 +0200

    SceneOne Button Created
    Canvas added
    Canvases prefabed
    Tested scene change

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void OnClick(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
