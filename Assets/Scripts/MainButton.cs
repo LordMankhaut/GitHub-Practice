f4cdc034abcc8e37ff21b7517ae024d79716ddb3
LordMankhaut <bdalcorn@outlook.com>
2018-05-11 20:56:11 +0200

    SceneOne Created
    Canvas added
    Panel added
    Button added
    Button switches to "Main" scene

---
+++ Assets/Scripts/MainButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainButton : MonoBehaviour
+{
+    public void OnClick(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
